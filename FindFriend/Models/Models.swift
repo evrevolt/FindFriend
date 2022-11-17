4c4bfa3f7ca7b4c8d5b776f8a715af8810bd1f05
Gennady Vedernikov <revoltlight@icloud.com>
2022-11-17 13:56:48 +0300

    Create DataStore

--- FindFriend/Models/Models.swift
+++ FindFriend/Models/Models.swift
@@ -9,70 +9,44 @@ import Foundation
 
 struct Pets {
     let name: String
-    let petsType: PetsType
+    let petsType: String
     let description: String
+    let phone: String
     let image: String
 }
 
-enum PetsType {
-    case cat
-    case dog
-}
 
-let pets = [
-    Pets(name: "Мурзик",
-         petsType: .cat,
-         description: "Этот котик попал к нам из мусорного бочка",
-         image: "cat1.jpg"),
-    
-    Pets(name: "Шарик",
-         petsType: .dog,
-         description: "Этот дружище бегал по улице в поисках еды",
-         image: "cat1.jpg"),
-    
-    Pets(name: "Мурзик",
-         petsType: .cat,
-         description: "Этот котик попал к нам из мусорного бочка",
-         image: "cat1.jpg"),
-    
-    Pets(name: "Мурзик",
-         petsType: .cat,
-         description: "Этот котик попал к нам из мусорного бочка",
-         image: "cat1.jpg"),
-    
-    Pets(name: "Мурзик",
-         petsType: .cat,
-         description: "Этот котик попал к нам из мусорного бочка",
-         image: "cat1.jpg"),
-    
-    Pets(name: "Мурзик",
-         petsType: .cat,
-         description: "Этот котик попал к нам из мусорного бочка",
-         image: "cat1.jpg"),
-    
-    Pets(name: "Мурзик",
-         petsType: .cat,
-         description: "Этот котик попал к нам из мусорного бочка",
-         image: "cat1.jpg"),
-    
-    Pets(name: "Мурзик",
-         petsType: .cat,
-         description: "Этот котик попал к нам из мусорного бочка",
-         image: "cat1.jpg"),
-    
-    Pets(name: "Мурзик",
-         petsType: .cat,
-         description: "Этот котик попал к нам из мусорного бочка",
-         image: "cat1.jpg"),
-    
-    Pets(name: "Мурзик",
-         petsType: .cat,
-         description: "Этот котик попал к нам из мусорного бочка",
-         image: "cat1.jpg")
-]
-
-func getPets() -> Pets {
-    
-    return pets.randomElement()!
-    
+extension Pets {
+    static func getPetsList() -> [Pets] {
+        let dataStore = DataStore.shared
+        var petsList: [Pets] = []
+        
+        let names = dataStore.names.shuffled()
+        let petsTypes = dataStore.petsTypes.shuffled()
+        let desctiprions = dataStore.descriptions.shuffled()
+        let phones = dataStore.phones.shuffled()
+        let images = dataStore.images.shuffled()
+        
+        let iterationCount = min(
+            names.count,
+            petsTypes.count,
+            desctiprions.count,
+            phones.count,
+            images.count
+        )
+        
+        for index in 0..<iterationCount {
+            let pets = Pets(
+                name: names[index],
+                petsType: petsTypes[index],
+                description: desctiprions[index],
+                phone: phones[index],
+                image: images[index]
+            )
+            
+            petsList.append(pets)
+        }
+        
+        return petsList
+    }
 }
---
+++ FindFriend/Services/DataStore.swift
@@ -0,0 +1,48 @@
+//
+//  DataStore.swift
+//  FindFriend
+//
+//  Created by Геннадий Ведерников on 17.11.2022.
+//
+
+import Foundation
+
+class DataStore {
+    
+    static let shared = DataStore()
+    
+    var names = [
+        "John", "Aaron", "Tim", "Ted",
+        "Steven", "Sharon", "Nicola",
+        "Allan", "Bruce", "Carl"
+    ]
+    
+    let petsTypes = [
+        "Cat", "Dog", "Cat", "Dot",
+        "Cat", "Dot", "Cat",
+        "Dog", "Cat", "Dog"
+    ]
+    
+    let descriptions = [
+        "jjjj@mail.ru", "aaaa@mail.ru", "eeee@mail.ru",
+        "hhhh@mail.ru", "wwww@mail.ru", "mmmm@mail.ru",
+        "xxxx@mail.ru", "pppp@mail.ru", "ssss@mail.ru",
+        "llll@mail.ru"
+    ]
+    
+    
+    let phones = [
+        "745396026", "145036843", "287185656", "293520954",
+        "562880225", "738594072", "825654134", "645898473",
+        "432349845", "459477625"
+    ]
+    
+    let images = [
+        "1.jpg", "1.jpg", "1.jpg", "1.jpg",
+        "1.jpg", "1.jpg", "1.jpg", "1.jpg",
+        "1.jpg", "1.jpg"
+    ]
+    
+    
+    private init() {}
+}
