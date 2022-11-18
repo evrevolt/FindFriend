1b7778e0f5222fb1835cee965f88c5064dd4f8a1
Asya Sher <asia.kondairy@gmail.com>
2022-11-18 13:37:26 +0300

    setup collectionViewCell

---
+++ FindFriend/ViewControllers/PetCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  PetCollectionViewCell.swift
+//  FindFriend
+//
+//  Created by Asya Sher on 18.11.2022.
+//
+
+import UIKit
+
+class PetCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var petImageView: UIImageView! {
+        didSet {
+            petImageView.layer.cornerRadius = 10
+        }
+    }
+    
+    @IBOutlet var petsLabel: UILabel!
+}
